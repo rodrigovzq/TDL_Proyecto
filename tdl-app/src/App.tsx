8c94df953b1334285cd7ede14a43456fc052051d
rodrigovzq <rvazquez@fi.uba.ar>
2022-12-13 11:06:32 -0300

    recetas oven

--- tdl-app/src/App.tsx
+++ tdl-app/src/App.tsx
@@ -175,16 +175,32 @@ function App() {
   const [utensils, setUtensils] = useState<utensil[]>([
     {
       name: 'oven',
-      combinations: [['tomato-sauce','dough', 'pre-pizza'],
-                    ['cheese','pre-pizza', 'pizza-simple'],
-                    ['potato', '', 'baked-potato'],
-                    ['dough', '', 'bread']],
+      combinations: [
+        ['dough', 'tomato-sauce', '', 'pre-pizza'],
+        ['cheese', 'pre-pizza', '', 'pizza-simple'],
+        ['onion', 'pre-pizza', '', 'pizza-onion'],
+        // Platos de 3 ingredientes
+        ['cheese', 'pre-pizza', 'tomato', 'pizza-tomato'],
+        ['cheese', 'onion', 'pre-pizza', 'pizza-onion'],
+        ['cheese', 'pre-pizza', 'meat,', 'pizza-meat'],
+        /////////////////////////////
+        ['potato', '', '', 'baked-potato'],
+        ['dough', '', '', 'bread'],
+        ['dough', 'egg', '', 'pasta'],
+        ['bread', 'meat', '', 'milanesa'],
+        ['meat', 'pasta', '', 'lasagna'],
+        ['meat', '', '', 'steak'],
+        ['cake-premix', '', '', 'cake'],
+        ['cake-premix', 'fruit', '', 'pastafrola'],
+        ['cake-premix', 'ddl', '', 'cake-ddl'],
+        ['cake-premix', 'carrot', '', 'cake-carrot'],
+      ],
       action: 'bake'
     },
     {
       name: 'pan',
-      combinations: [['egg','','fried-egg'],
-                    ['potato', '', 'french-fries']],
+      combinations: [['egg', '', 'fried-egg'],
+      ['potato', '', 'french-fries']],
       action: 'fry or grill'
     },
     {
@@ -194,15 +210,15 @@ function App() {
     },
     {
       name: 'bowl',
-      combinations: [['water','flour', 'dough'],
-                     ['water','tomato', 'tomato-sauce'], 
-                     ['water','water', 'glass-of-water'],
-                     ['fruit','sugar', 'fruit-salad'],
-                     ['fruit','water', 'juice'],
-                     ['fruit', 'milk', 'smoothie'],
-                     ['glass-of-water','fruit', 'juice'], 
-                     ['glass-of-water','sugar', 'soda-water'],
-                    ],
+      combinations: [['water', 'flour', 'dough'],
+      ['water', 'tomato', 'tomato-sauce'],
+      ['water', 'water', 'glass-of-water'],
+      ['fruit', 'sugar', 'fruit-salad'],
+      ['fruit', 'water', 'juice'],
+      ['fruit', 'milk', 'smoothie'],
+      ['glass-of-water', 'fruit', 'juice'],
+      ['glass-of-water', 'sugar', 'soda-water'],
+      ],
       action: 'combine'
     }
   ])
@@ -225,20 +241,20 @@ function App() {
   }
 
   const combineIngredients = (selectedIngredients: ingredient[]) => {
-    if (selectedIngredients.length===0) {
+    if (selectedIngredients.length === 0) {
       Alert.noIngredientSelected()
       return;
     }
     const combinations = selectedUtensil[0].combinations;
-    const selectedIngNames = [...selectedIngredients.map( (a) => a.name), '']
+    const selectedIngNames = [...selectedIngredients.map((a) => a.name), '']
 
     const result = combinations.find((value) => (
-      (value[0] === selectedIngNames[0] && value[1] === selectedIngNames[1])  ||
-            (value[0] === selectedIngNames[1] && value[1] === selectedIngNames[0])
+      (value[0] === selectedIngNames[0] && value[1] === selectedIngNames[1]) ||
+      (value[0] === selectedIngNames[1] && value[1] === selectedIngNames[0])
     ))
     console.log(result)
 
-    if(result !== undefined){
+    if (result !== undefined) {
       const resultName = result[2]
       const oldRecipe = recipes.find((ingredient) => (ingredient.name === resultName))
       if (oldRecipe === undefined) {
@@ -255,8 +271,8 @@ function App() {
         Alert.ingredientDiscovered(resultName)
       }
     }
-    else 
-    Alert.noCombination()
+    else
+      Alert.noCombination()
   }
 
   const cancelUtensil = () => {
@@ -272,20 +288,20 @@ function App() {
       <Welcome />
       <div className="AppName">Little Restaurant</div>
       <div className="progress">
-        Recipes Found: {`${recipes.filter((a) => (a.isAvailable && a.isInMenu )).length} / 
-                         ${recipes.filter((a)=> (a.isInMenu)).length}`}
+        Recipes Found: {`${recipes.filter((a) => (a.isAvailable && a.isInMenu)).length} / 
+                         ${recipes.filter((a) => (a.isInMenu)).length}`}
       </div>
       <div className="AppContent">
         <div className="workbenchEl">
-          <Workbench selectedUtensil={selectedUtensil} 
-                     selectedIngredients={selectedIngredients} 
-                     recipes={recipes.filter((a) => (a.isAvailable))}
-                     utensils={utensils}
-                     onChooseIngredient={chooseIngredient}
-                     onChooseUtensil={chooseUtensil}
-                     onCombine={combineIngredients}
-                     onCancelIngredients={cancelIngredients}
-                     onCancelUtensils={cancelUtensil}/>
+          <Workbench selectedUtensil={selectedUtensil}
+            selectedIngredients={selectedIngredients}
+            recipes={recipes.filter((a) => (a.isAvailable))}
+            utensils={utensils}
+            onChooseIngredient={chooseIngredient}
+            onChooseUtensil={chooseUtensil}
+            onCombine={combineIngredients}
+            onCancelIngredients={cancelIngredients}
+            onCancelUtensils={cancelUtensil} />
         </div>
         <div className="restaurantEl">
           <Menu menu={recipes.filter((a) => (a.isInMenu && a.isAvailable))} />
